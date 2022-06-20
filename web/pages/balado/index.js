76ed72055d57627b39515680e42a328d31923404
Andrew Nowacki <andrew.p.nowacki@gmail.com>
2022-06-20 15:02:07 -0400

    added default export to balado, attempt 2

--- web/pages/balado/index.js
+++ web/pages/balado/index.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import styled from "styled-components";
+import { client } from "../../lib/sanity/client";
+import { footerLogoQuery } from "../../lib/sanity/footerLogoQuery";
 
-const Balado = () => {
+export default function Balado() {
   return (
-    <div>Balado</div>
-  )
+    <Main>
+      <h2>h1</h2>
+    </Main>
+  );
 }
 
-export default Balado
+export async function getStaticProps() {
+  const footerLogos = await client.fetch(footerLogoQuery);
+
+  return {
+    props: {
+      footerLogos,
+    },
+    revalidate: 10,
+  };
+}
+
+const Main = styled.div`
+  background-color: var(--color-cream);
+  transition: var(--transition);
+`;
