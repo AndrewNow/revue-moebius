459ed65e182d35226d809badef7d7c6e79aaa531
Andrew Nowacki <andrew.p.nowacki@gmail.com>
2022-07-17 16:49:38 -0400

    testing marquee

--- web/components/bannerMarquee.js
+++ web/components/bannerMarquee.js
@@ -5,13 +5,13 @@ import { breakpoints } from "../utils/breakpoints";
 
 const BannerMarquee = ({ data }) => {
   return (
-    <Wrapper>
-      <Marquee gradientWidth={0} speed={55}>
+    <Marquee gradientWidth={0} speed={55}>
+      <Wrapper>
         <Link href={`${data.link}`}>
           <small>{data.title}</small>
         </Link>
-      </Marquee>
-    </Wrapper>
+      </Wrapper>
+    </Marquee>
   );
 };
 
