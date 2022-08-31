3a5025785f2f4d6283fdeff69debb75917b13c20
Andrew Nowacki <andrew.p.nowacki@gmail.com>
2022-08-31 12:26:52 -0400

    fixing button toggle

--- web/components/navbar/toggleDarkMode.js
+++ web/components/navbar/toggleDarkMode.js
@@ -7,58 +7,12 @@ import { useTheme } from "next-themes";
 
 const ToggleDarkMode = () => {
   // Set Dark mode / Light mode
-  // according to https://sreetamdas.com/blog/the-perfect-dark-mode
-
-  // __Rules__
-  // * If the user has visited our site before, then we use their saved preference.
-  // * If the user hasn't visited our site before or hasn't saved a preference, then we check if their Operating System has a preference and use the same.
-  // * If the above two methods don't return a preference still, then we default to a light theme.
-  // * All the above checks need to be run before our page is rendered/shown to the user.
-  // * Allow the user to toggle dark mode, and save their preference for future reference.
-
-  // const [darkTheme, setDarkTheme] = useState(undefined);
-
-  // const handleToggle = () => {
-  //   setDarkTheme(!darkTheme);
-  // };
-
-  // const storeUserSetPreference = (pref) => {
-  //   localStorage.setItem("theme", pref);
-  // };
-
-  // const root = document.documentElement;
-
-  // useEffect(() => {
-  //   if (document !== undefined) {
-  //     const initialColorValue = root.style.getPropertyValue(
-  //       "--initial-color-mode"
-  //     );
-  //     setDarkTheme(initialColorValue === "dark");
-  //   }
-  // }, []);
-
-  // useEffect(() => {
-  //   if (document !== undefined) {
-  //     // const root = document.documentElement;
-  //     if (darkTheme !== undefined) {
-  //       if (darkTheme) {
-  //         root.setAttribute("data-theme", "dark");
-  //         storeUserSetPreference("dark");
-  //       } else {
-  //         root.removeAttribute("data-theme");
-  //         storeUserSetPreference("light");
-  //       }
-  //     }
-  //   }
-  // }, [darkTheme]);
-
   const [mounted, setMounted] = useState(false);
   const { theme, setTheme } = useTheme();
 
   // When mounted on client, now we can show the UI
   useEffect(() => setMounted(true), []);
 
-  
   if (!mounted) return null;
 
   const handleToggle = () => {
@@ -68,23 +22,23 @@ const ToggleDarkMode = () => {
       setTheme("light");
     }
   };
-  
+
   return (
-    // darkTheme !== undefined && (
-    <>
-      <Desktop>
-        <Button onClick={handleToggle} aria-label="toggle dark mode">
-          <Circle />
-          <small>Mode {theme === "light" ? "sombre" : "claire"}</small>
-        </Button>
-      </Desktop>
-      <Mobile>
-        <Button onClick={handleToggle} aria-label="toggle dark mode">
-          {/* <LightDarkButton darkTheme={darkTheme} /> */}
-        </Button>
-      </Mobile>
-    </>
-    // )
+    theme !== undefined && (
+      <>
+        <Desktop>
+          <Button onClick={handleToggle} aria-label="toggle dark mode">
+            <Circle />
+            <small>Mode {theme === "dark" ? "sombre" : "claire"}</small>
+          </Button>
+        </Desktop>
+        <Mobile>
+          <Button onClick={handleToggle} aria-label="toggle dark mode">
+            <LightDarkButton darkTheme={theme === "dark"} />
+          </Button>
+        </Mobile>
+      </>
+    )
   );
 };
 
