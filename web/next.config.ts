e32d3c302eb404c6f2bb680838776a1667febb41
Andrew Nowacki <andrew.p.nowacki@gmail.com>
2022-09-30 08:46:59 -0400

    testing ts next config

--- web/next.config.ts
+++ web/next.config.ts
@@ -1,4 +1,8 @@
-module.exports = {
+/**
+ * @type {import('next').NextConfig}
+ */
+
+const nextConfig = {
   reactStrictMode: true,
   compiler: {
     // ssr and displayName are configured by default
@@ -27,3 +31,5 @@ module.exports = {
     ],
   },
 };
+
+module.exports = nextConfig
